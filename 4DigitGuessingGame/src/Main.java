248ae3dc2f2d398b312b03f47d51c8bf4833d400
Yiğit <yigit.ileri@ozu.edu.tr>
2021-07-26 14:18:21 +0300

    implemented scanner and random and wrote the initial message

--- 4DigitGuessingGame/src/Main.java
+++ 4DigitGuessingGame/src/Main.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class Main {
     public static void main (String[] args){
+        Scanner console = new Scanner(System.in);
+        Random rand = new Random();
 
+        int number = rand.nextInt(9000) + 1000;
+        System.out.println("I have chosen a random 4-digit-number. Try to guess it.");
+        int guess = console.nextInt();
     }
 }
